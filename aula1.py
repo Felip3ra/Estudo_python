8fb9a6bb4d72112a43f48667a4d41e5e354aa1a1
Felip3ra <88563642+Felip3ra@users.noreply.github.com>
2024-03-23 18:31:23 -0300

    upload: correcao

---
+++ aula1.py
@@ -0,0 +1,19 @@
+"""
+docstring
+
+comentario em multiplas linhas
+
+o python lê esse docstring
+"""
+
+'''
+dessa maneira tbm
+'''
+
+
+# comentario em python
+
+print(123) #na frente
+
+#abaixo
+print("sim")
