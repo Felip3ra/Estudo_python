0ee66b089ed818551f408ce48aebc0bb907d4472
Felip3ra <felipesantanasantos1812@gmail.com>
2024-03-23 20:01:42 -0300

    feat: aula 2

---
+++ aula2.py
@@ -0,0 +1,6 @@
+#\r \n -> CRLF
+#recebe argumentos
+#sep cria um separador
+#end cria o finalizador do print que pode ser modificado
+print(12,34, sep='-', end='##\n')
+print(56,78, end='\n')#separador padrão
