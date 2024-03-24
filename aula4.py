570d77e06a5ee19586d65dcd41d0e8129721b484
Felip3ra <felipesantanasantos1812@gmail.com>
2024-03-24 12:24:17 -0300

    feat:aula4

---
+++ aula4.py
@@ -0,0 +1,18 @@
+#tipos int e float
+#int -> numero inteiro
+#o tipo int representa qualquer numero
+#positivo ou negativo
+print(11) #int
+
+
+#float -> numero com ponto flutuante
+#o tipo float representa qualquer numero
+#positivo ou negativo com ponto flutuante
+print(1.1,10.11)#float
+print(0.0,-1.5)
+
+#a função type mostra o tipo que o python
+#inferiu ao valor
+print(type(1.1))
+print(type(1))
+print(type('felipe'))
