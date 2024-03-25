79cd276343e5724af3cf4af95520d4a20367722e
Felip3ra <felipesantanasantos1812@gmail.com>
2024-03-24 22:50:11 -0300

    feat:aula8

---
+++ aula8.py
@@ -0,0 +1,13 @@
+nome = 'Felipe'
+sobrenome = 'Santana'
+idade = 22
+ano = 2024 - idade
+maior_de_idade = idade >= 18
+altura = 1.85
+
+print('nome:',nome)
+print('sobrenome:',sobrenome)
+print('idade:',idade)
+print('ano de nascimento:',ano)
+print('É maior?',maior_de_idade)
+print('altura: ',altura)
