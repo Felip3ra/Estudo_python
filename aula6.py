946b7d8a524744d4b51798592906eebf9d675b41
Felip3ra <felipesantanasantos1812@gmail.com>
2024-03-24 22:35:16 -0300

    feat:aula6

---
+++ aula6.py
@@ -0,0 +1,15 @@
+#conversão de tips, coerção
+#type convertion, typecasting, coercion
+#é o ato de converter um tipo em outro
+#tipos imutáveis e primitivos:
+#str, int, float, bool
+
+print(1+1)
+print('a' + 'b')
+
+print(int('1') + 1)#converte str em int
+print(float('1.2') + 1)#converte str em float
+print(bool(''))#converte em bool
+print(bool('frase'))#converte em bool
+
+
