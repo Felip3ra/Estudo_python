ed25da41e088d8656120a2b3e7d85aea3cae570d
Felip3ra <felipesantanasantos1812@gmail.com>
2024-03-25 23:24:26 -0300

    feat:aula15

---
+++ aula15.py
@@ -0,0 +1,6 @@
+nome = input('qual é o seu nome?')
+numero1 = int(input('digite um numero: '))
+numero2 = int(input('digite um segundo numero: '))
+
+
+print(f'o seu nome é {nome} e a soma é {numero1+numero2}')
