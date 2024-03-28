f6855ee07cf5ad6cfc29cb0127f9403b05e3b37d
Felip3ra <felipesantanasantos1812@gmail.com>
2024-03-27 22:38:02 -0300

    feat:aula16

---
+++ aula16.py
@@ -0,0 +1,11 @@
+#if / elif   / else
+#se / se não se / se não
+
+entrada = input('voce quer entrar ou sair? ')
+
+if entrada == 'entrar':
+    print('voce entrou')
+elif entrada == 'sair':
+    print('voce saiu do sistema')
+else:
+    print('nenhuma das opções foram selecionadas...')
